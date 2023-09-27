f6dc80ae9fc1fdbc4b19e6f53dfc48e9bfd0399b
rnvelasco <rnvelasco@yahoo.com>
2023-09-27 19:45:33 +0800

    Mini Calculator Changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
+import React from 'react';
+import { useState } from "react";
 import './App.css';
 
-function CalcButton({label}) {
+function MyButton({label, onClick}) {
   return (
-    <button className="CalcButton">
+    <button className="CalcButton" onClick ={onClick}>
       {label}
     </button>
   );
 }
 
-function CalcDisplay({display}) {
+function Display({display}) {
   return (
-    <div className='CalcDisplay'>
+    <div className="CalcDisplay">
       {display}
-    </div>
+    </div>    
   );
 }
 
-function App() {
+export default function App() {
+
+  const[disp, setDisp] = useState(0);
+  const[num1, setNum1] = useState(null);
+  const[oper, setOper] = useState(null);
+  const[num2, setNum2] = useState(null);
+
+
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    var num = value;
+    if(oper === null) {
+      if(num1 !== null) {
+        num = num1 + num;
+      }
+      setNum1(num);            
+      setDisp(num);            
+    } else {
+      if(num2 !== null) {
+        num = num2 + num;
+      }
+      setNum2(num);            
+      setDisp(num);       }
+  }
+
+  const operatorClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    setOper(value);
+    setDisp(value);
+  }
+
+  const equalClickHandler = (e) => {
+    e.preventDefault();
+
+    if (oper === "+") {
+      setDisp(parseInt(num1) + parseInt(num2));
+    } else {
+      setDisp("ERROR");
+    }
+  }
+
+  const clearClickHandler = (e) => {
+    e.preventDefault();
+
+    setDisp(0);
+    setNum1(null);
+    setOper(null);
+    setNum2(null);
+  }
+
+  const nameClickHandler = (e) => {
+    e.preventDefault();
+    // Action to display your name
+    alert("Display your name in the calculator display");
+  }
+
   return (
-    <div className='CalcContainer'>
-      <CalcDisplay display={"Hello World"}/>
-      <div className='ButtonContainer'>
-        <CalcButton label={7}/>
-        <CalcButton label={8}/>
-        <CalcButton label={9}/>
-        <CalcButton label={"+"}/>
-        <CalcButton label={4}/>
-        <CalcButton label={5}/>
-        <CalcButton label={6}/>
-        <CalcButton label={"-"}/>
-        <CalcButton label={1}/>
-        <CalcButton label={2}/>
-        <CalcButton label={3}/>
-        <CalcButton label={"*"}/>
-        <CalcButton label={"C"}/>
-        <CalcButton label={0}/>
-        <CalcButton label={"="}/>
-        <CalcButton label={"/"}/>
+    <div className="App">
+      <h1>Mini Calculator</h1>
+      <Display display={disp} />
+      <div className="ButtonContainer">
+        <MyButton label={1} onClick={numberClickHandler}/>
+        <MyButton label={2} onClick={numberClickHandler}/>
+        <MyButton label={"C"} onClick={clearClickHandler}/>
+        <MyButton label={3} onClick={numberClickHandler}/>
+        <MyButton label={4} onClick={numberClickHandler}/>
+        <MyButton label={"+"} onClick={operatorClickHandler}/>
+        <MyButton label={5} onClick={numberClickHandler}/>
+        <MyButton label={6} onClick={numberClickHandler}/>
+        <MyButton label={"="} onClick={equalClickHandler}/>
+      </div>
+      <div>
+        <MyButton label={"VELASCO"} onClick={nameClickHandler}/>
       </div>
     </div>
-  );}
-
-export default App;
+  );
+}
